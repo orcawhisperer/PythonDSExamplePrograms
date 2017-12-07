33d394de31fddd2df69351988521e6bea9fd718b
terminator <kvasanth373@gmail.com>
2017-12-08 01:19:39 +0530

    Initial commit

---
+++ Chapter5/mergesort.py
@@ -0,0 +1,43 @@
+def mergeSort(alist):
+    print("Splitting ",alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i<len(lefthalf) and j<len(righthalf):
+            if lefthalf[i]<righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i<len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j<len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",alist)
+    
+def main():
+    size =  int(input('Enter Size of List to be sorted :'))
+    alist = []
+    for i in range(size):
+        alist.append(int(input()))
+    print(alist)
+    mergeSort(alist)
+    print(alist)
+if __name__ == '__main__':
+    main()
